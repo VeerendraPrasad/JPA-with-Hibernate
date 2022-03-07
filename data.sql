a957d4318f30a0645fba1244826f3771ed15a87f
VeerendraPrasad <100115450+VeerendraPrasad@users.noreply.github.com>
2022-03-07 05:26:38 +0000

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,25 @@
+insert into employee(id, name, last_name) 
+values(100,'sirisha','siri');
+insert into employee (id, name, last_name) 
+values(101,'neelima' ,'bandaru' );
+insert into employee(id, name, last_name ) 
+values(102,'surya', 'chandhra');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
